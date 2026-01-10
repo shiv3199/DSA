0d5a8f8eae5dbb2adc3beb6bd851036f9562ee31
shivam <shivam.maurya.3199@gmail.com>
2026-01-10 11:53:40 +0530

    largest number find number in array

---
+++ findLargest.js
@@ -0,0 +1,11 @@
+let arr=[1,5,48,56,7]
+function largestNumber(array){
+    let large=0;
+    for(i=0; i<array.length; i++){
+        if(array[i]>large){
+            large = array[i]
+        }
+    }
+    return large
+}
+console.log(largestNumber(arr))
