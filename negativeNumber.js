6e4542fb48ddc3627454523b415ac57b7e6aaf29
shivam <shivam.maurya.3199@gmail.com>
2026-01-10 11:42:54 +0530

    Find negative number

--- negativeNumber.js
+++ negativeNumber.js
@@ -1,11 +1,11 @@
-let arr = [2,3,4,5,8,-7]
-function negativeNumber(arr,num){
-
-for (i=0; i<arr.length; i++){
-    if(arr[i]<0){
-        return i
+let arr = [2, 3, 4, 5, 8, -7, -4, -5, -8];
+function negativeNumber(arr) {
+  let count = 0;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      count +=1;
     }
+  }
+  return count ;
 }
-return 0
-}
-console.log(negativeNumber(arr,-7))
+console.log(negativeNumber(arr));
