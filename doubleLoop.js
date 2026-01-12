649db193e2c935e16acc52199fe6b3138831d330
shivam <shivam.maurya.3199@gmail.com>
2026-01-12 12:05:46 +0530

    this is Double loop Exercise

---
+++ doubleLoop.js
@@ -0,0 +1,12 @@
+for (i=0; i<5; i++){
+    for(j=0; j<5; j++){
+        console.log(`value of i ${i}, value of j ${j}`)
+    }
+}
+
+// varition
+for (i=5; i>0; i--){
+    for (j=0; j<i; j++){
+        console.log(`Value of i ${i} and Value of j ${j}`)
+    }
+}
